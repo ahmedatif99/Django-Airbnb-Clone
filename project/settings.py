bdf13b34b450927e61f2b9cea942fc7980ee5522
ahmedatif99 <ahmedahmed1919a@gmail.com>
2022-12-19 13:58:58 +0200

    edit allow hosts

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-fasgv=y(og#knroa^%=6&8wnhdgika&8@9y#(+x2=r%p)lr$xz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '*']
+ALLOWED_HOSTS = ['ahmedatif99.pythonanywhere.com', '127.0.0.1', '*']
 
 
 # Application definition
